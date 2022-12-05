2bd4658587bb4778eeeca90d8a3f900d51a1813f
Timo Ufermann <timo.ufermann@googlemail.com>
2022-12-05 09:41:10 +0100

    included black and flake8

--- app.py
+++ app.py
@@ -6,31 +6,32 @@ from fastapi import FastAPI, HTTPException, Depends, status, responses, File, Up
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 import secrets
 
-#importing own modules
+# importing own modules
 from module_template import module_class
 
-#MY_ENV_VAR = os.getenv('MY_ENV_VAR')
+# MY_ENV_VAR = os.getenv('MY_ENV_VAR')
 
-#reading potential config
+# reading potential config
 config = ConfigParser()
 config.read("config/conf.conf")
 
-if 'AM_I_IN_A_DOCKER_CONTAINER' not in os.environ:
+if "AM_I_IN_A_DOCKER_CONTAINER" not in os.environ:
     load_dotenv()
-    
-#Reading necessary info from environment or config
-user_name = os.environ['USER_NAME']
-password = os.environ['USER_PASSWORD']
-sample_file = config['GENERAL']['SAMPLE_FILE']
+
+# Reading necessary info from environment or config
+user_name = os.environ["USER_NAME"]
+password = os.environ["USER_PASSWORD"]
+sample_file = config["GENERAL"]["SAMPLE_FILE"]
 
 
-#Initializing FASTAPI APP
+# Initializing FASTAPI APP
 app = FastAPI()
 security = HTTPBasic()
 
+
 def has_access(credentials: HTTPBasicCredentials = Depends(security)):
     """
-        Function that is used to validate the username in the case that it requires it
+    Function that is used to validate the username in the case that it requires it
     """
     correct_username = secrets.compare_digest(credentials.username, user_name)
     correct_password = secrets.compare_digest(credentials.password, password)
@@ -43,30 +44,41 @@ def has_access(credentials: HTTPBasicCredentials = Depends(security)):
 
     return True
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
 
 @app.get("/secured/")
 async def secured_function(access: bool = Depends(has_access)):
     new_object = module_class.sample_class()
     result = new_object.sample_method()
-    return {"Access" : result}
+    return {"Access": result}
+
 
 @app.get("/sendfile/")
 async def send_file():
     return responses.FileResponse(sample_file)
 
+
 @app.post("/postfile/")
-async def post_file(file_to_process: UploadFile = File(...), access: bool = Depends(has_access)):
+async def post_file(
+    file_to_process: UploadFile = File(...), access: bool = Depends(has_access)
+):
     file_location = f"files/{file_to_process.filename}"
     print(file_location)
     with open(file_location, "wb+") as file_object:
         file_object.write(file_to_process.file.read())
 
-    return {"message" : 'file_sent'}
+    return {"message": "file_sent"}
+
 
 if __name__ == "__main__":
     uvicorn.run(
-    "app:app", 
-    host="0.0.0.0", port=int(os.environ['UVICORN_PORT']), log_level="debug", reload="true")
+        "app:app",
+        host="0.0.0.0",
+        port=int(os.environ["UVICORN_PORT"]),
+        log_level="debug",
+        reload="true",
+    )
